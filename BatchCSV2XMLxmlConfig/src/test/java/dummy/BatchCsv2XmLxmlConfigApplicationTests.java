2202daf3e357c0112214605a98267f1eb2b0d3d3
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2021-05-22 09:20:27 +0530

    Add files via upload
    BatchCSV2XMLxml

---
+++ BatchCSV2XMLxmlConfig/src/test/java/dummy/BatchCsv2XmLxmlConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package dummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchCsv2XmLxmlConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
